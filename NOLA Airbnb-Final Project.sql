dfd8694c4e26d72fad2e50b9e613daef84ea1bd0
Trevanti93 <168140047+Trevanti93@users.noreply.github.com>
2024-07-11 10:57:33 -0400

    Update NOLA Airbnb-Final Project.sql

--- NOLA Airbnb-Final Project.sql
+++ NOLA Airbnb-Final Project.sql
@@ -1,6 +1,8 @@
 -- Selecting Airbnb Database for use--
 use air_bnb_db;
 
+-- QA/Data Cleaning--
+
 -- Check total rows--
 select count(*) from nola_listings;
 
@@ -41,8 +43,8 @@ MODIFY COLUMN ID BIGINT;
 
 select
 	host_id,
-    host_name,
-    count(id) as listing_count
+    	host_name,
+    	count(id) as listing_count
 from
 nola_listings
 group by host_id, host_name
@@ -52,7 +54,7 @@ limit 10;
 -- Average Price For 1-5 Bedrooms and Studio--
 select
 	case when bedrooms = 0 then 'Studio/Bedrooms Not Listed' else bedrooms end as bedrooms,
-    round(avg(price),0) as avg_price_per_night
+    	round(avg(price),0) as avg_price_per_night
 from
 nola_listings
 where bedrooms in (1, 2, 3, 4, 5,'Studio/Bedrooms Not Listed')
@@ -62,8 +64,8 @@ order by 2 desc;
 -- Top 10 Areas with Highest Avg Price Per Night--
 select
 	neighbourhood_cleansed as neighborhood,
-    round(avg(price),0) as avg_price,
-    count(id) as number_of_listings
+    	round(avg(price),0) as avg_price,
+    	count(id) as number_of_listings
 from
 nola_listings
 group by 1
@@ -72,12 +74,12 @@ limit 10;
 
 -- Neighbourhoods with Highest Projected Monthly Revenue--
 with projected_revenue as
-				(select id,
-						host_id,
-                        neighbourhood_cleansed AS neighborhood,
-                        price * (30 - availability_30) as projected_30_day_revenue,
-                        id as listing_id
-					from nola_listings
+			(select id,
+				host_id,
+                        	neighbourhood_cleansed AS neighborhood,
+                        	price * (30 - availability_30) as projected_30_day_revenue,
+                        	id as listing_id
+			from nola_listings
                     order by projected_30_day_revenue desc)
 select
     neighborhood,
@@ -90,20 +92,19 @@ limit 10;
 
 -- Avg price per neighborhood--
 with neighboorhood_price as (select
-	neighbourhood_cleansed as neighborhood,
-    round(avg(price),0) as avg_price,
-    count(*) as number_of_listings
-from
-nola_listings
-group by 1
-order by 2 desc)
+				neighbourhood_cleansed as neighborhood,
+    				round(avg(price),0) as avg_price,
+    				count(*) as number_of_listings
+			from nola_listings
+			group by 1
+			order by 2 desc)
 
 select
 	np.neighborhood,
 	nl.latitude,
-    nl.longitude,
-    np.avg_price,
-    np.number_of_listings
+    	nl.longitude,
+   	np.avg_price,
+    	np.number_of_listings
 from neighboorhood_price np
 	join
 nola_listings nl on np.neighborhood = nl.neighbourhood_cleansed
@@ -114,12 +115,12 @@ order by 4 desc;
 
 -- Occupancy Rate By Neighbourhood--
 with occ_rate as
-				(select id,
-						host_id,
+		(select id,
+			host_id,
                         neighbourhood_cleansed,
                         30 - availability_30 as booked_days,
                         price * (30 - availability_30) as projected_30_day_revenue
-					from nola_listings
+		from nola_listings
                     order by booked_days desc)
 select
     neighbourhood_cleansed,
@@ -133,13 +134,13 @@ order by 3 desc;
 
 -- Top 10 Neighborhoods with highest Occupancy Rate--
 with occ_rate as
-				(select id,
-						host_id,
+		(select id,
+			host_id,
                         neighbourhood_cleansed,
                         30 - availability_30 as booked_days,
                         price * (30 - availability_30) as projected_30_day_revenue
-					from nola_listings
-                    order by booked_days desc)
+		from nola_listings
+                order by booked_days desc)
 select
     neighbourhood_cleansed,
     round(avg(booked_days),2) as avg_booked_days,
@@ -152,14 +153,14 @@ order by 3 desc
 limit 10;
 
 
--- Ratings by Neighbourhood with listings between 100 and 500 --
+-- Ratings by Neighbourhood with total listings between 100 and 500 --
 select
 	neighbourhood_cleansed,
-    round(avg(review_scores_cleanliness),2) as cleanliness_rating,
-    round(avg(review_scores_location),2) as location_Rating,
-    round(avg(review_scores_value),2) as value_Rating,
-    round((avg(review_scores_cleanliness) + avg(review_scores_location) + avg(review_scores_value))/3,2) as overall_rating,
-    count(*) as total_listings
+    	round(avg(review_scores_cleanliness),2) as cleanliness_rating,
+    	round(avg(review_scores_location),2) as location_Rating,
+    	round(avg(review_scores_value),2) as value_Rating,
+    	round((avg(review_scores_cleanliness) + avg(review_scores_location) + avg(review_scores_value))/3,2) as overall_rating,
+    	count(*) as total_listings
 from nola_listings
 group by neighbourhood_cleansed
 having total_listings > 99 and total_listings < 501
@@ -168,11 +169,11 @@ order by 5 desc;
 -- Ratings by Neighbourhood with all total listings--
 select
 	neighbourhood_cleansed,
-    round(avg(review_scores_cleanliness),2) as cleanliness_rating,
-    round(avg(review_scores_location),2) as location_Rating,
-    round(avg(review_scores_value),2) as value_Rating,
-    round((avg(review_scores_cleanliness) + avg(review_scores_location) + avg(review_scores_value))/3,2) as overall_rating,
-    count(*) as total_listings
+    	round(avg(review_scores_cleanliness),2) as cleanliness_rating,
+    	round(avg(review_scores_location),2) as location_Rating,
+    	round(avg(review_scores_value),2) as value_Rating,
+    	round((avg(review_scores_cleanliness) + avg(review_scores_location) + avg(review_scores_value))/3,2) as overall_rating,
+    	count(*) as total_listings
 from nola_listings
 group by neighbourhood_cleansed
 order by 5 desc;
